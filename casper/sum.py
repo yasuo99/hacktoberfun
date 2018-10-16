09a2002d57cd8ba24553ddb63df1e3b58265fe0d
joshedewa <joshedewa@gmail.com>
2018-10-16 12:11:54 +0300

    Added edewajosh folder

---
+++ casper/sum.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+def sum(x, y):
+    return x + y
+
+if __name__ == "__main__":
+    a = float(input("First number : "))
+    b = float(input("Second number : "))
+
+    print(sum(a,b))
