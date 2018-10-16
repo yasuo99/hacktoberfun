42af301ff7cd807ba455e9370536e3835aedbb5a
Gurpreet Singh <gurpreet15140@iiitd.ac.in>
2018-10-16 17:57:35 +0530

    Added sum function to gps08
    * added sum.py
    
    * added sum.cpp

---
+++ kbc/sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namepsace std;
+
+int main(){
+	int a,b;
+	cout<<"Enter a number: "; 
+	cin>>a;
+	cout<<"Enter another number: ";
+	cin>>b;
+	cout<<"sum is "<<a+b<<endl;
+	return 0;
+}
