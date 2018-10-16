908bbc1214fc98478a56476da71ee5b461d4e170
Abigail Arias <mabigail1123@gmail.com>
2018-10-16 03:07:29 -0600

    Added bgail folder

---
+++ bgail/sum.py
@@ -0,0 +1,9 @@
+def sum(x,y):
+	return x+y;
+def main():
+    x = int(input('Enter first num: '))
+    y = int(input('Enter second num: '))
+    print(sum(x,y))
+
+if __name__ == '__main__':
+    main()
