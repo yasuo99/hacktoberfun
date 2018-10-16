700088a3e8a124487224ceba41fb8767bfbe7d40
adarsh1498 <44162669+adarsh1498@users.noreply.github.com>
2018-10-16 17:57:52 +0530

    Added some function to adarsh1498
    * even odd program to determine a number
    
    * sum of two no.

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N1,N2,result;
+    
+    cout << "Enter two integers: ";
+    cin >> N1 >> N2;
+
+    result = N1 + N2;
+    
+    cout << N1 << " + " <<  N2 << " = " << result;     
+
+    return 0;
+}
